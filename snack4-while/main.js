d1f1a2efc38fb4eca618e63de9ed82d2af35823f
Rosario Garufi <saro.garufi1@gmail.com>
2022-03-31 15:41:37 +0200

    Consegna esercizio 4

--- snack4-while/main.js
+++ snack4-while/main.js
@@ -4,21 +4,20 @@ Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in 
 
 */
 
-let numeri = [2, 4, 5, 6, 8, 11, 13, 18 , 19, 23, 26, 27 , 1, 3, 13, 15, 29];
+let numeri = [2, 5, 7, 11, 29, 90];
 
 let sum = 0;
-let i = 0;
 
-while(i<numeri.length){
+
+for(let i = 0; i<numeri.length; i++){
     
-    numero_dispari = numeri.length[i] %2!=0;
-    if(numeri.length[i] %2 != 0){
-        console.log(numero_dispari);
+    if(i %2 != 0){
+        console.log(i);
+        sum += numeri[i];
     }
     
-    
-    i++;
-
 }
 
+
 console.log(`La somma dei numeri dispari è ${sum}`);
+
